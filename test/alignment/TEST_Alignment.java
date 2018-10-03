7e1f6fe0628b29e677aaaad6c0c840472d1426ba
Benjamin Linard <benjamin.linard@lirmm.fr>
2018-10-03 16:54:46 +0200

    -put back tests

---
+++ test/alignment/TEST_Alignment.java
@@ -0,0 +1,150 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package alignment;
+
+import alignement.Alignment;
+import core.AAStates;
+import core.DNAStatesShifted;
+import core.States;
+import etc.Infos;
+import inputs.FASTAPointer;
+import inputs.Fasta;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author ben
+ */
+public class TEST_Alignment {
+
+    
+    public static void main(String[] args) {
+        
+        States s=new DNAStatesShifted();
+        System.setProperty("debug.verbose", "1"); // Print Infos.out
+        
+        Fasta f1=new Fasta("1", "-ATC-TG--GT---");
+        Fasta f2=new Fasta("2", "-A-C-T---GT---");
+        Fasta f3=new Fasta("3", "-AT--T-C-GT---");
+        
+        List<Fasta> align=new ArrayList<>(); 
+        align.add(f1);
+        align.add(f2);
+        align.add(f3);
+        
+        System.out.println("HERE !");
+        
+        Alignment a=new Alignment(s,align);
+        
+        System.out.println(a.describeAlignment(true));
+        System.out.println("ORIGINAL:");
+        a.printAlignment();
+        System.out.println(Arrays.toString(a.getGapProportions()));
+        System.out.println(Arrays.toString(a.getGapIntervals()));
+        
+        a.addSequence("4", new String("-A---TCC-GT--A").toCharArray());
+        System.out.println("ADD 1");
+        a.printAlignment();
+        System.out.println(Arrays.toString(a.getGapProportions()));
+        System.out.println(Arrays.toString(a.getGapIntervals()));
+        
+        System.out.println("ADD 2");
+        String[] labels={"5","6"};
+        ArrayList<char[]> seqs= new ArrayList<>();
+        seqs.add(new String("-A---TCC-GT--A").toCharArray());
+        seqs.add(new String("AAT---CA-GT-AA").toCharArray());
+        a.addAllSequences(labels, seqs);
+        a.printAlignment();
+        System.out.println(Arrays.toString(a.getGapProportions()));
+        System.out.println(Arrays.toString(a.getGapIntervals()));
+
+        System.out.println("REMOVE 1");
+        a.removeSequence("3");
+        a.printAlignment();
+        System.out.println(Arrays.toString(a.getGapProportions()));
+        System.out.println(a.describeAlignment(true));
+        System.out.println(Arrays.toString(a.getGapIntervals()));
+
+        System.out.println("REDUCTION");
+        a.reduceAlignment(0.80);
+        a.printAlignment();
+        System.out.println(Arrays.toString(a.getGapProportions()));
+        System.out.println(a.describeAlignment(true));
+        System.out.println(Arrays.toString(a.getGapIntervals()));
+        
+        
+        System.out.println("END");
+        System.out.println("-----------------------------------------------------");
+
+        Infos.println("Loading Alignment...");
+        FASTAPointer fp=new FASTAPointer(new File("/home/ben/Dropbox/viromeplacer/test_datasets/ancestral_reconstruct_tests/paml/pplacer_refpkg/vaginal_16s_ORIGINAL/bv_refs_aln_stripped_99.5.fasta"), false);
+        Fasta fasta=null;
+        ArrayList<Fasta> fastas=new ArrayList<>();
+        while ((fasta=fp.nextSequenceAsFastaObject())!=null) {
+            fastas.add(fasta);
+        }
+        a=new Alignment(s,fastas);
+        System.out.println(a.describeAlignment(true));
+        double[] gapProportions1 = a.getGapProportions();
+        for (int i = 0; i < gapProportions1.length; i++) {
+            double d = gapProportions1[i];
+            System.out.println("i:"+i+ " prop="+d);
+        }
+        a.reduceAlignment(0.97);
+//        gapProportions1 = a.getGapProportions();
+//        for (int i = 0; i < gapProportions1.length; i++) {
+//            double d = gapProportions1[i];
+//            System.out.println("i:"+i+ " prop="+d);
+//        }
+        System.out.println(a.describeAlignment(true));
+        fp.closePointer();
+        
+        System.exit(1);
+        
+        try {
+            a.writeAlignmentAsFasta(new File("/home/ben/test.fasta"));
+        } catch (IOException ex) {
+            Logger.getLogger(Alignment.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        System.out.println("------------");
+        Alignment a2=a.copy();
+        gapProportions1 = a2.getGapProportions();
+//        for (int i = 0; i < gapProportions1.length; i++) {
+//            double d = gapProportions1[i];
+//            System.out.println("i:"+i+ " prop="+d);
+//        }
+        a2.removeSequence("SDH3502-15.matK_Rosales_1-790/1-790");
+        System.out.println(a2.describeAlignment(true));
+        gapProportions1 = a2.getGapProportions();
+//        for (int i = 0; i < gapProportions1.length; i++) {
+//            double d = gapProportions1[i];
+//            System.out.println("i:"+i+ " prop="+d);
+//        }
+        a2.removeSequence("DBMPP221-14.matK_Rosales_1-784/1-784");
+        System.out.println(a2.describeAlignment(true));
+        gapProportions1 = a2.getGapProportions();
+//        for (int i = 0; i < gapProportions1.length; i++) {
+//            double d = gapProportions1[i];
+//            System.out.println("i:"+i+ " prop="+d);
+//        }
+        System.out.println("Reduce at 1.0");
+        a2.reduceAlignment(1.0);
+        System.out.println(a2.describeAlignment(true));
+        
+        try {
+            a2.writeAlignmentAsFasta(new File("/home/ben/test2.fasta"));
+        } catch (IOException ex) {
+            Logger.getLogger(Alignment.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+}
